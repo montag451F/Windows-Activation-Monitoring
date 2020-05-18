884ab7bf90001b9504854deb781f6d41e51620f1
montag451F <montag616@gmail.com>
2020-05-18 22:54:55 +0700

    Update start_GetActivation.ps1

--- start_GetActivation.ps1
+++ start_GetActivation.ps1
@@ -1,1 +1,1 @@
-PowerShell.exe -ExecutionPolicy Bypass -File C:\zabbix\bin\win64\GetWinActivationStatus.ps1 | %{ $_.Split(':')[1]; } | ForEach-object { $_.TrimStart(" ") } 2> $null > C:\zabbix\bin\win64\activation.txt
+PowerShell.exe -ExecutionPolicy Bypass -File C:\WindowsActivationStatus\GetWinActivationStatus.ps1 | %{ $_.Split(':')[1]; } | ForEach-object { $_.TrimStart(" ") } 2> $null > C:\zabbix\bin\win64\activation.txt
