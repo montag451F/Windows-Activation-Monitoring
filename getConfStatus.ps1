4bc70f1c3edd509a9d7b74bd54b664a743a88aa4
montag451F <montag616@gmail.com>
2020-05-28 04:12:51 +0700

    Update getConfStatus.ps1

--- getConfStatus.ps1
+++ getConfStatus.ps1
@@ -1,1 +1,1 @@
-Select-String -Path "C:\Program Files\Zabbix Agent\zabbix_agentd.conf" -Pattern "activation" | Select Line | Format-List
+Select-String -Path C:\zabbix\conf\zabbix_agentd.win.conf -Pattern "activation" | Select Line | Format-List
